6c76c546385814cf8a48fd8716886ddb19b14977
vivihm <viviana.marca@sptech.school>
2023-10-02 12:26:15 -0300

    criação da estrututra do projeto

---
+++ src/api.js
@@ -0,0 +1,13 @@
+//importando a biblioteca que faz requisições http (precisa de menos coisa do que comparado ao fetch)
+import axios from "axios";
+
+//criando instancia do axios
+//agora qualquer requisição a api é feito como api.Verbo() 
+const api = axios.create({
+    baseURL: "" //colocar o link da api que tem caminho para todas (a Gateway)
+    });
+
+//exportamos para poder acessar em todas as paginas do projeto
+export default api;
+
+    
