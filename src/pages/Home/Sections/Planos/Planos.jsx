fde941554bd9e72b57ca0ab69457bb7729034114
Viviana Marca <viviana.marca@sptech.school>
2023-11-27 20:30:11 -0300

    alterações institucional

--- src/pages/Home/Sections/Planos/Planos.jsx
+++ src/pages/Home/Sections/Planos/Planos.jsx
@@ -19,41 +19,69 @@ const Planos = () => {
       </div>
 
       <div className="cardPlanos-container">
-        <div className='cardPlanos'>
-          <p className='plano-categoria'>Free</p><div className='linha-fina'></div>
-          <h2>Gratuito</h2>
-          <p className='p1'>Plano Padrão</p>
-          <p className='p2'>Ideal para buscar serviços para sua empresa.</p>
-          <p className='p3'><img src={Check} alt="" />Acesso a portfolios de empresas certificadas</p>
-          <p className='p3'> <img src={Check} alt="" />Filtros de busca de serviços</p>
-          <p className='p3'><img src={Check} alt="" />Intermediação de contato </p>
-          <button className='botao-preenchido-planos'>Escolher Plano</button>
-        </div>
-        <div className='cardPlanos'>
-          <p className='plano-categoria'>Analytics</p>
-          <div className='linha-fina'></div>
-          <h2>R$ 29,90/mês</h2>
-          <p className='p1'>Plano Anual</p>
-          <p className='p2' >Ideal para buscar serviços e analisar o crescimento ESG na sua empresa.</p>
-          <p className='p3'> <img src={Check} alt="" />Benefícios do Plano Free</p>
-          <p className='p3'> <img src={Check} alt="" />Acesso ao formulário ESG</p>
-          <p className='p3'> <img src={Check} alt="" />Área com gráficos que mostra o crescimento ESG na sua empresa</p>
+
+        <div className='borda-espacada-container-plano'>
+
+          <div className='cardPlanos'>
+            <p className='plano-categoria'>Free</p><div className='linha-fina'></div>
+            <h2 className='preco-plano-institucional'> <span className='preco-institucional'>Gratuito</span></h2>
+            <p className='p1'>Plano Padrão</p>
+            <p className='p2'>Ideal para buscar serviços para sua empresa.</p>
+
+            <div className='descricao-plano-institucional'>
+              <p className='p3'><img src={Check} alt="" /> Acesso a portfolios de empresas certificadas</p>
+              <p className='p3'> <img src={Check} alt="" />Filtros de busca de serviços</p>
+              <p className='p3'><img src={Check} alt="" />Intermediação de contato </p>
+            </div>
             <button className='botao-preenchido-planos'>Escolher Plano</button>
+          </div>
         </div>
-        <div className='cardPlanos'>
-          <p className='plano-categoria'>Provider</p>
-          <div className='linha-fina'></div>
-          <h2>R$ 49,90/mês</h2>
-          <p className='p1'>Plano Anual</p>
-          <p className='p2'>Permite a criação de um portfolio da sua empresa na plataforma. </p>
-          <p className='p3'><img src={Check} alt="" />Benefícios do Plano Free</p>
-          <p className='p3'><img src={Check} alt="" />Criação de Potfolio</p>
-          <p className='p3'><img src={Check} alt="" />Intermediação de contato com as empresas contratantes</p>
-          <button className='botao-preenchido-planos'>Solicitar Avaliação</button>
+
+
+        
+        <div className='borda-espacada-container-plano'>
+
+          <div className='cardPlanos'>
+            <p className='plano-categoria'>Analytics</p>
+            <div className='linha-fina'></div>
+            <h2 className='preco-plano-institucional'>R$ <span className='preco-institucional'>29,90</span>/mês</h2>
+            <p className='p1'>Plano Anual</p>
+
+            <div className="descricao-plano-institucional">
+
+
+              <p className='p2' >Ideal para buscar serviços e analisar o crescimento ESG na sua empresa.</p>
+              <p className='p3'> <img src={Check} alt="" /> Benefícios do Plano Free</p>
+              <p className='p3'> <img src={Check} alt="" />Acesso ao formulário ESG</p>
+              <p className='p3'> <img src={Check} alt="" />Área com gráficos que mostra o crescimento ESG na sua empresa</p>
+            </div>
+            <button className='botao-preenchido-planos'>Escolher Plano</button>
+          </div>
+        </div>
+
+
+        <div className='borda-espacada-container-plano'>
+
+          <div className='cardPlanos'>
+            <p className='plano-categoria'>Provider</p>
+            <div className='linha-fina'></div>
+            <h2 className='preco-plano-institucional'>R$ <span className='preco-institucional'>49,90</span>/mês</h2>
+            <p className='p1'>Plano Anual</p>
+            <p className='p2'>Permite a criação de um portfolio da sua empresa na plataforma. </p>
+
+            <div className="descricao-plano-institucional">
+
+              <p className='p3'><img src={Check} alt="" />Benefícios do Plano Free</p>
+              <p className='p3'><img src={Check} alt="" />Criação de Potfolio</p>
+              <p className='p3'><img src={Check} alt="" />Intermediação de contato com as empresas contratantes</p>
+            </div>
+            <button className='botao-preenchido-planos'>Solicitar Avaliação</button>
+          </div>
+        </div>
+
         </div>
       </div>
-    </div>
-  );
+      );
 };
 
-export default Planos;
+      export default Planos;
